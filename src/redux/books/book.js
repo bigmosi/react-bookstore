95440fec7db173bdb0f7169f70c1cabca0c72f2e
bigmosi <kinyeramo@gmail.com>
2022-07-13 20:28:12 +0300

    fetched data using external api

--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,23 +1,13 @@
+import axios from 'axios';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Hunger Games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: '2',
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: '3',
-    title: 'Capital in The Twenty-First Century',
-    author: 'Suzanne Collins',
-  },
-];
+const FETCH_BOOK = 'bookstore/books/FETCH_BOOK';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hr3W4s4GHxpRX0UTPNXs/books';
+
+const initialState = [];
 
 export const addBookAction = (payload) => ({
   type: ADD_BOOK,
@@ -28,6 +18,37 @@ export const removeBookAction = (payload) => ({
   type: REMOVE_BOOK,
   payload,
 });
+
+const fetchBook = (payload) => ({
+  type: FETCH_BOOK,
+  payload,
+});
+
+export const fetchBookApiAction = () => async (dispatch) => {
+  const books = await axios.get(url);
+  const bookFetched = Object.entries(books.data).map((item) => {
+    const { title, author } = item[1][0];
+    return { id: item[0], title, author };
+  });
+  dispatch(fetchBook(bookFetched));
+};
+
+export const addBookApiAction = (payload) => async (dispatch) => {
+  const { id, title, author } = payload;
+  const newBook = {
+    item_id: id,
+    title,
+    author,
+    category: 'Fiction',
+  };
+  await axios.post(url, newBook);
+  dispatch(addBookAction(payload));
+};
+
+export const removeBookApiAction = (id) => async (dispatch) => {
+  await axios.delete(`${url}/${id}`);
+  dispatch(removeBookAction({ id }));
+};
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
